2f1a298e5c6e18aee4aec4f5a924aa29f4eb584e
t-meki <corbic273@yahoo.co.jp>
2016-05-19 18:05:13 +0900

    POST　受取　製作中

--- 02/contact.php
+++ 02/contact.php
@@ -1,52 +1,58 @@
 <head>
     <meta charset="UTF-8">
-    <!-- <title></title>
-    <link rel="stylesheet" href="form_styles.css">-->
+    <title>PhpFormContact</title>
+    <link rel="stylesheet" href="form_styles.css">
 </head>
 <body>
+    <h1>お問い合わせフォーム</h1>
     <form action="result.php" method="post">
     <p>
-        姓　<input type="text" name="txt" size="20">
-        名　<input type="text" name="txt" size="20">
+        ・姓　<input type="text" name="ｆay" size="20">
     </p>
     <p>
-        性別
-        <input type="radio" name="rdo" value="c1">男
-        <input type="radio" name="rdo" value="c2">女
-        <input type="radio" name="rdo" value="c3">不明
+         名　<input type="text" name="name" size="20">
     </p>
     <p>
-        住所　<input type="text" name="txt" size="20">
-        名　<input type="text" name="txt" size="20">
+        ・性別
+        <input type="radio" name="sex" value="c1">男
+        <input type="radio" name="sex" value="c2">女
+        <input type="radio" name="sex" value="c3">不明
     </p>
     <p>
-        電話番号　<input type="text" name="txt" size="5">
-        -<input type="text" name="txt" size="5">
-        -<input type="text" name="txt" size="5">
-    </p>
-        メールアドレス　<input type="text" name="txt" size="20">
-        ＠　<input type="text" name="txt" size="20">
+        ・住所　<input type="text" name="address" size="20">
     </p>
     <p>
-        どこで知ったか
-        <input type="checkbox" name="cdx" value="c1">ネット
-        <input type="checkbox" name="cdx" value="c2">口コミ
-        <input type="checkbox" name="cdx" value="c3">街角
-        <input type="checkbox" name="cdx" value="c4">その他
+        ・電話番号　<input type="text" name="tel[]" size="5">
+        -<input type="text" name="tel[]" size="5">
+        -<input type="text" name="tel[]" size="5">
+    </p>
+        ・メールアドレス　<input type="mail[]" name="mail" size="20">
+        ＠　<input type="text" name="mail[]" size="20">
     </p>
     <p>
-        質問カテゴリ
-    <select name="prefectures">
+        ・どこで知ったか
+        <input type="checkbox" name="cdx[]" value="c1">ネット
+        <input type="checkbox" name="cdx[]" value="c2">口コミ
+        <input type="checkbox" name="cdx[]" value="c3">街角
+        <input type="checkbox" name="cdx[]" value="c4">新聞・雑誌等
+        <input type="checkbox" name="cdx[]" value="c5">その他
+    </p>
+    <p>
+        ・質問カテゴリ
+    <select name="category">
         <option>HTMLとPHPの対応について</option>
         <option>CSSでのページ整型について</option>
         <option>PHPでの定数について</option>
         <option>その他</option>
     </select>
     </p>
-    質問内容　
-    <textarea cols="30" rows="5" name="comments">
-    </textarea>
-
+    <p>
+        ・質問内容
+    </p>
+    <p>　
+        <textarea cols="30" rows="5" name="contents">
+        </textarea>
+    </p>
     <p>
     <input type="submit" value="送信">
 
---
+++ 02/result.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <title>PhpFormResult</title>
+    <link rel="stylesheet" href="form_styles.css">
+</head>
+<body>
+    <h2>お問い合わせ完了</h2>
+
+    <p>姓名</p>
+        <?php
+            var_dump ($_POST) ;
+        ?>
+
+
+
+</body>
