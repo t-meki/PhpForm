e9fb6ddb6525953b22d22fd5cc28ab415c6963f8
t-meki <corbic273@yahoo.co.jp>
2016-05-23 10:55:31 +0900

    整列完了　受け取り不可

--- 02/contact.php
+++ 02/contact.php
@@ -9,46 +9,65 @@
 </header>
 <body>
     <form action="result.php" method="post">
-    <dl>
-        <dt>・姓</dt>　
-        <dd><input type="text" name="name1" size="20"></dd>
-    <dt>・名</dt> <dd><input type="text" name="name2" size="20"></dd>
-    <dt>・性別</dt>
-        <dd>
+    <table rules="none">
+    <tr>
+        <td>・姓</td>　
+        <td><input type="text" name="name1" size="20"></td>
+    </tr>
+    <tr>
+        <td>・名</td>
+        <td><input type="text" name="name2" size="20"></td>
+    </tr>
+    <tr>
+        <td>・性別</td>
+        <td>
             <input type="radio" name="sex" value="c1">男
             <input type="radio" name="sex" value="c2">女
             <input type="radio" name="sex" value="c3">不明
-        </dd>
-    <dt>・住所</dt>
-        <dd><input type="text" name="address" size="20"></dd>
-    <dt>・電話番号</dt>　
-        <dd>
+        </td>
+    </tr>
+    <tr>
+        <td>・住所</td>
+        <td><input type="text" name="address" size="20"></td>
+    </tr>
+    <tr>
+        <td>・電話番号</td>　
+        <td>
             <input type="text" name="tel[]" size="5">
             - <input type="text" name="tel[]" size="5">
             - <input type="text" name="tel[]" size="5">
-        </dd>
-    <dt>・メールアドレス</dt>
-        <dd>
+        </td>
+    </tr>
+    <tr>
+        <td>・メールアドレス</td>
+        <td>
             <input type="text" name="mail1" size="20">
-            @　<input type="text" name="mail2" size="20">
-        </dd>
-    <dt>・どこで知ったか</dt>
-        <dd>
+             @ <input type="text" name="mail2" size="20">
+        </td>
+    </tr>
+    <tr>
+        <td>・どこで知ったか&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td>
             <input type="checkbox" name="cdx[]" value="c1">ネット
             <input type="checkbox" name="cdx[]" value="c2">口コミ
             <input type="checkbox" name="cdx[]" value="c3">街角
             <input type="checkbox" name="cdx[]" value="c4">新聞・雑誌等
             <input type="checkbox" name="cdx[]" value="c5">その他
-        </dd>
-    <dt>・質問カテゴリ</dt>
-        <dd>
+        </td>
+    </tr>
+    <tr>
+        <td>・質問カテゴリ</td>
+        <td>
             <select name="category">
                 <option>HTMLとPHPの対応について</option>
                 <option>CSSでのページ整型について</option>
                 <option>PHPでの定数について</option>
                 <option>その他</option>
             </select>
-        </dd>
+        </td>
+    </tr>
+    </table>
+
     <p>・質問内容<p>
     <p>　
         <textarea cols="70" rows="5" name="contents" ></textarea>
